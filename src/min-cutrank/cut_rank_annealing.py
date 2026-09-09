08d81a8971246c87b21c16920bba03c1c764dee8
Kjell Fredrik Pettersen <kjellfredrik.pettersen@sintef.no>
2026-09-09 11:20:22 +0200

    Annealing algorithm: For each row, swap only happens once among columns with optimal swap cut-rank

--- src/min-cutrank/cut_rank_annealing.py
+++ src/min-cutrank/cut_rank_annealing.py
@@ -13,6 +13,8 @@ def cut_rank_annealing_direct(partition : GraphPartition, temperatures, log: boo
     cut_rank = partition.cut_rank
     nmb_rows = len(rows)
     nmb_cols = len(cols)
+    row_ranks = [0] * nmb_cols
+
     if log:
         print(f"Starting with cut-rank {cut_rank}")
 
@@ -23,16 +25,18 @@ def cut_rank_annealing_direct(partition : GraphPartition, temperatures, log: boo
             for j in range(nmb_cols):
 
                 rows[i], cols[j] = cols[j], rows[i]
-
                 copy_matrix(partition.adjacencies, partition.buffer, rows, cols)
                 base_rows, _ = rank_matrix_positions(partition.buffer, rows, cols)
-                new_cut_rank = len(base_rows)
-                delta_rank = new_cut_rank - cut_rank
+                row_ranks[j] = len(base_rows)
+                rows[i], cols[j] = cols[j], rows[i]
 
-                if delta_rank <= 0 or random.random() < limits[delta_rank - 1]:
-                    cut_rank = new_cut_rank
-                else:
-                    rows[i], cols[j] = cols[j], rows[i]
+            min_cutrank=min(row_ranks)
+            if min_cutrank <= cut_rank or random.random() < limits[min_cutrank - cut_rank - 1]:
+                min_idx = [j for j in range(nmb_cols) if row_ranks[j] == min_cutrank]
+                j = random.choice(min_idx)
+                partition.apply_swap(rows[i], cols[j])
+                rows[i], cols[j] = cols[j], rows[i]
+                cut_rank = min_cutrank
 
         if log:
             print(f"Cut-rank is {cut_rank} after sweep with temperature {temp}")
@@ -57,20 +61,14 @@ def cut_rank_annealing_row_formula(partition : GraphPartition, temperatures, log
             for n in partition.nodes:
                 row_ranks[n] = -1
             row_swap_cut_ranks(partition, row, row_ranks)
-            swap_col = -1
-            for j in range(nmb_cols):
+            min_cutrank=min(row_ranks[cols[j]] for j in range(nmb_cols))
+            if min_cutrank <= cut_rank or random.random() < limits[min_cutrank - cut_rank - 1]:
+                min_idx = [j for j in range(nmb_cols) if row_ranks[cols[j]] == min_cutrank]
+                j = random.choice(min_idx)
+                partition.apply_swap(row, cols[j])
+                rows[i], cols[j] = cols[j], rows[i]
+                cut_rank = min_cutrank
 
-                new_cut_rank = row_ranks[cols[j]]
-                if new_cut_rank < 0:
-                    raise Exception("Cut-rank not calculated")
-                delta_rank = new_cut_rank - cut_rank
-                if delta_rank <= 0 or random.random() < limits[delta_rank - 1]:
-                    swap_col = cols[j]
-                    rows[i], cols[j] = cols[j], rows[i]
-                    cut_rank = new_cut_rank
-
-            if swap_col >= 0:
-                partition.apply_swap(row, swap_col)
             if partition.cut_rank != cut_rank:
                 raise Exception("Partition cut-rank does not fit with directly calculated rank")
 
